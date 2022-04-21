9ad06b033cd3a540b5d758ebcbc866e5311b2dca
inayaka <ina4@wp.eu>
2022-04-21 17:04:59 +0200

    zmiany_20042022

--- index.js
+++ index.js
@@ -1,12 +1,88 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.get('/', function (req, res) {
-  console.log(req)
-  res.send('Hello World')
-})
+// POBIERZ FILE SYSTEM
+const fs = require('fs'); 
 
-app.listen(80)
+// 1) Create our store
+// console.log(__dirname)
+// console.log(__filename)
+
+// app.use(express.static('src/static'))
+
+// KONFIGURACJA STATYCZNEGO FOLDERU
+app.use
+  (express.static(
+      path.join(__dirname, '/src/static')
+      )
+  )
+  
+      // POBIERANIE INFORMACJI OD KLIENTA (Z FORMULARZA)
+  app.get('/sendForm', (req, res) => {
+      // CZYTAMY PLIK DATA.JSON I ZAPISUJEMY JAKO OLDCONTENT
+      // OLDCONTENT JEST STRIGIEM
+      // console.log(req.query)
+    const oldContent = fs.readFileSync('data.json', {encoding:'UTH-8'}, (err, data) => {
+    
+    const newData =JSON.parse(oldContent);
+    newData.push(req.query)
+    
+
+
+    // fs.readFile('data.json', (err, data) => {
+    //   console.log(data)
+    // })
+    
+      
+      
+      res.send(data)
+    }) 
+
+
+    // fs.open('data.json', 'w', function (err, file) {
+    //   // if (err) throw err;
+    //   console.log('Saved!');
+    // });
+
+      res.send('okok')
+  })
+
+  // app.post('/sendForm', (req, res) => {
+  //   console.log(req.body)
+    
+
+    app.get('/getComments', (req, res) =>{           
+      res.send("okidoki")
+    })  
+
+    app.listen(80)
+
+
+  
+// app.get('*', funkction (req, res) {
+//   console.log(req.originalUrl)
+// })
+
+// app.get('/index.html', (req, res) => {
+//   res.sendFile
+// })
+
+
+// app.get('/style.css', (req, res) => {
+//   res.sendFile
+// })
+
+
+
+// app.get('/', function (req, res) {
+//   console.log(req)
+//   res.send('Heloooł świecie')
+// })
+
+// pliki statyczne
+
+
 
 
 
@@ -16,6 +92,5 @@ app.listen(80)
 // for(let letter of "Hello World Node JS"){
 //     console.log(letter);
 // }
-
 
 // console.log("Hello World Node");
